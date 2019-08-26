e7a3dd1fd2f6ff40fd2b51e89e57a1591a1d3a1e
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-08-26 23:04:39 +0900

    Updates

---
+++ precache-manifest.1f30b203660e3ad852b8748267842ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b210f6604164b9eb88d12517cdb31743",
+    "url": "/yoroi-extension-ledger-connect/index.html"
+  },
+  {
+    "revision": "1f63599fbe9cbb443e46",
+    "url": "/yoroi-extension-ledger-connect/static/css/main.40da5861.chunk.css"
+  },
+  {
+    "revision": "8739d0e406b13d141518",
+    "url": "/yoroi-extension-ledger-connect/static/js/2.420b25a6.chunk.js"
+  },
+  {
+    "revision": "1f63599fbe9cbb443e46",
+    "url": "/yoroi-extension-ledger-connect/static/js/main.d6d0c512.chunk.js"
+  },
+  {
+    "revision": "4279b4e8f6114fc53947",
+    "url": "/yoroi-extension-ledger-connect/static/js/runtime~main.0166cd4e.js"
+  }
+]);
