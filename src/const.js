7919d2813934cdba85d09f40764ee41e11a81034
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-10-23 14:02:13 +0900

    increased TRANSPORT_EXCHANGE_TIMEOUT_MS to 2 min

--- src/const.js
+++ src/const.js
@@ -12,4 +12,4 @@ export const ENV = {
 export const VIDEO_LINK_NANO_S = 'https://www.youtube.com/watch?v=YwdBLh7qAsI&t=2s';
 export const VIDEO_LINK_NANO_X = 'https://www.youtube.com/watch?v=YwdBLh7qAsI&t=2s';
 export const DEVICE_LOCK_CHECK_TIMEOUT_MS = 500; // In milli-seconds
-export const TRANSPORT_EXCHANGE_TIMEOUT_MS = 60000; // In milli-seconds
+export const TRANSPORT_EXCHANGE_TIMEOUT_MS = 120000; // In milli-seconds
